4ba45689da8a0d1095cafdad8a5dfc4a9e582d5b
shivansh6979 <shivansh11g@gmail.com>
2023-02-05 20:20:14 +0530

    configured firestore and chat component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
+import { signOut } from "firebase/auth";
 import { useRef, useState } from "react";
 import Cookies from "universal-cookie";
 import "./App.css";
 import Auth from "./components/Auth";
 import Chat from "./components/Chat";
+import { auth } from "./firebase-config";
 
 const cookies = new Cookies();
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(cookies.get("Auth-Token"));
   const [room, setRoom] = useState(null);
   const roomInputRef = useRef(null);
+
+  const signOutHandler = async () => {
+    await signOut(auth);
+    cookies.remove("Auth-token");
+    setIsLoggedIn(false);
+    setRoom(null);
+  };
+
   if (!isLoggedIn) {
     return (
       <div className="App">
@@ -19,7 +29,7 @@ function App() {
     return (
       <div>
         {room ? (
-          <Chat />
+          <Chat room={room} />
         ) : (
           <div>
             <label>Enter your Chat room :</label>
@@ -29,6 +39,9 @@ function App() {
             </button>
           </div>
         )}
+        <div>
+          <button onClick={signOutHandler}>Sign Out</button>
+        </div>
       </div>
     );
 }
--- frontend/src/components/Chat.js
+++ frontend/src/components/Chat.js
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import {
+  addDoc,
+  collection,
+  onSnapshot,
+  query,
+  serverTimestamp,
+  where,
+  orderBy,
+} from "firebase/firestore";
+import { auth, db } from "../firebase-config";
 
-const Chat = () => {
-  return <div>Chat</div>;
+const Chat = (props) => {
+  const [newMessages, setNewMessages] = useState("");
+  const { room } = props;
+  const messagesRef = collection(db, "messages");
+  const [messageUpdate, setMessageUpdate] = useState([]);
+
+  useEffect(() => {
+    const queryMessages = query(
+      messagesRef,
+      where("room", "==", room),
+      orderBy("createdAt")
+    );
+    const unsubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages = [];
+      snapshot.forEach((doc) => {
+        messages.push({ ...doc.data(), id: doc.id });
+      });
+      setMessageUpdate(messages);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (newMessages === "") return;
+    await addDoc(messagesRef, {
+      text: newMessages,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser.displayName,
+      room: room,
+    });
+    setNewMessages("");
+  };
+
+  return (
+    <div>
+      <h1>{room.toUpperCase()}</h1>
+      <div>
+        {messageUpdate.map((msg, index) => {
+          return (
+            <div key={index}>
+              <h2>{msg.user}</h2>
+              <h5>{msg.text}</h5>
+            </div>
+          );
+        })}
+      </div>
+      <form onSubmit={submitHandler}>
+        <input
+          onChange={(e) => setNewMessages(e.target.value)}
+          placeholder="Enter your message here"
+          value={newMessages}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;
